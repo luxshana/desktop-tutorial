226464a040468f8f7d94cf3f0db7ee0992262c4d
luxshana <96429939+luxshana@users.noreply.github.com>
2021-12-30 07:55:30 +0530

    Create lab.java

---
+++ lab.java
@@ -0,0 +1,16 @@
+public class Lab {
+
+    public static void main(String[] args) {
+	 int a;
+        int b;
+        int c;
+        Scanner input = new Scanner(System.in);
+		 System.out.println("enter your first number:");
+               a=input.nextInt();
+        System.out.println("enter your second number:");
+        b=input.nextInt();
+        c=a+b;
+        System.out.println("sum is:"+c);
+    }
+}
+    
